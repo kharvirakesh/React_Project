e4ee80c22fbe93442551625f72b1d705418ff0b2
kharvirakesh <kharvirakesh3@gmail.com>
2021-04-08 10:50:03 +0530

    outputting burger ingreedient dynamically

--- burger-builder/src/Container/BurgerBuilder/BurgerBuilder.js
+++ burger-builder/src/Container/BurgerBuilder/BurgerBuilder.js
@@ -3,12 +3,25 @@ import Aux from '../../hoc/Aux'
 import Burger from '../../Components/Burger/Burger'
 
 export class BurgerBuilder extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+              ingredients: {
+                cheese: 2,
+                bacon: 1,
+                meat: 2,
+                salad: 1
+            }
+        }
+    }
+
     render() {
         return (
             <Aux>
                 <div className='content'>
-                   <Burger />
-                   <p>Builder Controls</p>
+                    <Burger ingreedients={this.state.ingredients} />
+                    <p>Builder Controls</p>
                 </div>
             </Aux>
         )
